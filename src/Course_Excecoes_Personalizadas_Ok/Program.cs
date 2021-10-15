e290bd5c9f2a53fcf2ae0e61e9c02dbc717293fc
RaulORios <raul.orios89@outlook.com>
2021-10-15 14:04:09 -0300

    Arquivos atualizados

---
+++ src/Course_Excecoes_Personalizadas_Ok/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Course_Excecoes_Personalizadas_Ok.Entities;
+using Course_Excecoes_Personalizadas_Ok.Entities.Exceptions;
+
+namespace Course_Excecoes_Personalizadas_Ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine($"Reservation: {reservation}");
+                                
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexcpected error: " + e.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
